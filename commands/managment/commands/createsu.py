f7095262f2ad3373554babdccd27a410528d1109
Christian Tong <christian.tongcruz96@outlook.com>
2023-03-08 10:49:45 -0500

    Commands

--- commands/managment/commands/createsu.py
+++ commands/managment/commands/createsu.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(username='chris').exists():
             User.objects.create_superuser(
                 username='chris',
                 password='942946713'
